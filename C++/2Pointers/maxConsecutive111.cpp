18ec56684b52785783152225ed2fe25a405a4c82
Timo <timo.michaelis1@gmail.com>
2025-12-23 12:15:05 +0800

    fixed max Consecutive Ones solution

--- C++/2Pointers/maxConsecutive111.cpp
+++ C++/2Pointers/maxConsecutive111.cpp
@@ -3,33 +3,29 @@ using namespace std;
 
 class Solution {
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        int subarray_len = 0; 
-        int fast,slow = 0; 
-        int current_flip = 0; 
-        int max= 0; 
-        while(fast < nums.size()){ // 1,1,1,0,0,0,1,1,1,1,0
-            if(nums[fast] == 0 && current_flip == k){
-                max = subarray_len>max? subarray_len:max;
-                slow = fast;
-                subarray_len = 0; 
-                current_flip = 0; 
-            }else if(nums[fast] == 0 && current_flip < k){
-                current_flip++; 
-                subarray_len++; 
-                fast++; 
-            }else if(nums[fast]==0 && current_flip == 0){
-                slow = fast; 
-                fast++; 
-                subarray_len++; 
-                current_flip++; 
-            }else{
-                subarray_len++; 
-                fast++; 
+   int longestOnes(vector<int>& nums, int k) {
+    int forward = 0; 
+    int behind = 0; 
+    int count = 0; 
+    int max = 0; 
+        while(forward < nums.size()){
+            if (nums[forward] == 1){
+                count++;
+            }else if (k > 0){
+                k--;
+                count++;
+            }else {
+                while(behind < forward && nums[behind] == 1){
+                    count--; 
+                    behind++; 
+                }
+                behind++; 
             }
+            forward++; 
+            max = max < count? count : max; 
         }
-        max = subarray_len>max? subarray_len:max;
-        return max; 
+        max = max < count? count : max; 
+    return max; 
     }
 };
 
